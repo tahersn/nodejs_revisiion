860a02bc22a5516b4d82894de6c2d4d85e3d2c18
Taher <taher.snoussi@esprit.tn>
2023-05-21 15:30:53 +0200

    crud

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,6 +10,43 @@ async function create( req,res){
     }
 }
 
+async function deleteUser (req,res){
+    try{
+        await User.findByIdAndRemove(req.params.id)
+        res.send("user deleted !")
+    }catch(error){
+        console.log(error);
+    }
+}
+async function GetAll (req,res){
+    try{
+       const data =  await User.find();
+        res.send(data);
+    }catch(error){
+        res.send(error.message);
+    }
+}
+async function getbyid (req,res){
+    try{
+        const data = await User.findById(req.params.id);
+        res.send(data)
+    }catch(error){
+        console.log(error.message);
+    }
+}
+async function update(req,res){
+    try {
+        await User.findByIdAndUpdate(req.params.id,req.body,{new:true});
+        res.send("updated ! ");
+    }catch(error){
+        console.log(error.message);
+    }
+}
+
 module.exports= {
-    create
+    create,
+    deleteUser,
+    GetAll,
+    getbyid,
+    update
 }
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -4,5 +4,10 @@ const userController = require("../controllers/userController")
 
 
 router.post('/add',userController.create);
+router.delete('/delete/:id',userController.deleteUser);
+router.get('/getAll',userController.GetAll);
+router.get('/getOne/:id',userController.getbyid);
+router.put('/updateOne/:id',userController.update);
+
 
 module.exports = router ;
